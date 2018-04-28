bc4875f1016f604ab10d65d9e05838490161fe72
himanshuchawla009 <himanshuchawla2014@gmail.com>
2018-04-28 16:28:42 +0530

    api functions added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+var bodyParser = require('body-parser');
 var stellar = require('./stellar');
-
+app.set("view engine","jade");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/stellar',stellar)
 
 app.get('/',(req,res)=>{
-    res.send("home page");
+    res.sendFile('views/index.html',{root: __dirname });
+})
+app.post('/account',(req,res)=>{
+    console.log(req.body)
 })
 app.listen(3000,()=>{
+   
     console.log("listening")
 })
--- stellar.js
+++ stellar.js
@@ -1,29 +1,154 @@
-var bodyParser = require('body-parser');
-var stellar = require('stellar-sdk');
 
+var StellarSdk = require('stellar-sdk');
+var rp = require('request-promise');
 var express = require('express');
 var router = express.Router();
+var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
+StellarSdk.Network.useTestNetwork();
+
+
+var transaction;
+
 createAccount = (req,res)=>{
-    res.send("creating account");
+    console.log("creating account");
+    console.log();
+    var options = {
+        method: 'GET',
+        uri: 'https://friendbot.stellar.org',
+        qs: { addr:req.body.publickey},
+        json: true
+        
+    };
 
+     rp(options)
+    .then(function (parsedBody) {
+        // POST succeeded...
+        server.loadAccount(req.body.publickey)
+        .then(function(account) {
+            
+            account.balances.forEach(function(balance) {
+              res.render('accountInfo',{AssetType:balance.asset_type ,Balance: balance.balance});
+            });
+          });
+    })
+    .catch(function (err) {
+        // POST failed...
+        console.log(err);
+        res.send(err);
+        
+    })
 }
+
+
 payment = (req,res)=>{
-    res.send("sending payment");
+    var sourceKeys = StellarSdk.Keypair
+  .fromSecret(req.body.secret);
+    server.loadAccount(req.body.destinationKey)
+    .catch(StellarSdk.NotFoundError, function (error) {
+      throw new Error('The destination account does not exist!');
+    })
+    .then(function() {
+      return server.loadAccount(sourceKeys.publicKey());
+    })
+    .then(function(sourceAccount) {
+      transaction = new StellarSdk.TransactionBuilder(sourceAccount)
+        .addOperation(StellarSdk.Operation.payment({
+          destination: req.body.destinationKey,
+          asset: StellarSdk.Asset.native(),
+          amount: req.body.amount
+        }))
+        .addMemo(StellarSdk.Memo.text('Test Transaction'))
+        .build();
+      transaction.sign(sourceKeys);
+      return server.submitTransaction(transaction);
+    })
+    .then(function(result) {
+      console.log('Success! Results:', result);
+      server.loadAccount(sourceKeys.publicKey())
+        .then(function(account) {
+            account.balances.forEach(function(balance) {
+              res.render('accountInfo',{AssetType:balance.asset_type ,Balance: balance.balance});
+            });
+          });
+    })
+    .catch(function(error) {
+      console.error('Something went wrong!', error);
+    });
 
 }
-getHistory = (req,res)=>{
-    res.send("getting history");
 
-},
-getReceivedPayments = (req,res)=>{
-    res.send("getting payments")
+
+
+getReceivedPayments = (req,res,)=>{
+  var payments = server.payments().forAccount(req.query.accountId);
+    
+  payments.stream({
+    onmessage: function(payment) {
+      
+      if (payment.to !== req.query.accountId) {
+        
+        return;
+      }
+      
+      var asset;
+      if (payment.asset_type === 'native') {
+        asset = 'lumens';
+      }
+      else {
+        asset = payment.asset_code + ':' + payment.asset_issuer;
+      }
+      console.log(payment.amount + ' ' + asset + ' from ' + payment.from);
+      
+      
+    },
+   
+    onerror: function(error) {
+      console.error('Error in payment stream');
+    },
+   
+
+  });
+
 
 }
-router.get('/',(req,res)=>{
-    res.send(` <h1> api endpoints for get method: </h1> <br> <h2> /history </h2> <br> <h2> /receivedPayment </h2> <br> 
-    <h1> api endpoints for post method: </h1> <br> <h2> /createAccount </h2> <br> <h2> /sendPayment </h2>`)
+
+
+
+router.get('/publickey',(req,res)=>{
+    var pair = StellarSdk.Keypair.random();
+    let secret = pair.secret();
+    let public = pair.publicKey();
+    res.send("Secret:" + secret + "<br> Public:" + public +
+    `<br> 
+    <br>
+    <h2> copy your public key and click on create account to create new account </h2>
+    <br>
+    <form method='GET' action = '/stellar/createAccount'> 
+    <input type = 'submit' value='create account'/>
+    </form>`
+);
 });
-router.get('/history',getHistory);
+router.get('/createAccount',(req,res)=>{
+    res.sendFile('views/account.html',{root: __dirname });
+});
 
-router.get('/receivedPayment',getReceivedPayments);
+router.get('/sendPayment',(req,res)=>{
+  res.render('payment',{title:"Send payment"});
+});
+
+
+router.get('/receipts',getReceivedPayments);
+
+router.get('/receivedPayment',(req,res)=>{
+    res.render('receivedPayments');
+});
+
+
+router.post('/account',createAccount);
+
+router.post('/payment',payment);
+
+
+ 
+
 module.exports = router;
